f474f9c5778db9591b0817d33aeae444cdf62ca2
edraslazo <vl20011@ues.edu.sv>
2023-06-06 00:12:49 -0600

    nuevos elementos

---
+++ Js/_IniciarSesion.js
@@ -0,0 +1,42 @@
+const formulario = document.forms['form'];
+
+export const iniciarSesion = (e) => {
+	e.preventDefault();
+
+	const datos = {
+		nombreUsuario: formulario.user.value,
+		contraseña: formulario.password.value,
+	};
+
+    const elementos = JSON.parse(localStorage.getItem('usuario')) || [];
+	elementos.forEach((element) => {
+		if (element.nombreUsuario === datos.nombreUsuario) {
+			if (element.contraseña === datos.contraseña) {
+				alert('coinciden las credenciales');
+
+				const cadena = datos.nombreUsuario;
+				const palabra = 'moderador';
+
+				function buscarPalabra(cadena, palabra) {
+					var palabras = cadena.split(' '); // Divide la cadena en palabras separadas
+
+					if (palabras.includes(palabra)) {
+						return true;
+					} else {
+						return false;
+					}
+				}
+
+				if (buscarPalabra(cadena, palabra)) {
+					window.location.href = 'moderador.html';
+				} else {
+					window.location.href = 'usuario.html';
+				}
+			} else {
+				console.log('La contrasela es incorrecta');
+			}
+		} else {
+			console.log('El correo no coincide');
+		}
+	});
+};
--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,8 @@
 import { enviarDatos } from './_crearCuenta.js';
+import { iniciarSesion } from './_IniciarSesion.js';
 
 const crearCuenta = document.getElementById('sign__buttons-submit');
+const iniciarSesion = document.getElementById('login__buttons-submit');
 
 crearCuenta.addEventListener('click', enviarDatos);
+iniciarSesion.addEventListener('click', iniciarSesion);
