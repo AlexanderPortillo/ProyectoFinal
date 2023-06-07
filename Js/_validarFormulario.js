dd84101a40a443a27bc55606907fc0ffb087537f
Denis Martel <96342422+AlexPor22@users.noreply.github.com>
2023-06-06 21:42:10 -0600

    nuevos elementos

--- Js/_validarFormulario.js
+++
@@ -1,3 +0,0 @@
-const expresionRegularNombre = /^(?!.*\s\s)[a-zA-Z]{1}[a-zA-Z\s]{0,18}[a-zA-Z]{1}$/;
-const expresionRegularCorreo = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
-const expresionRegularContraseña = /^(?!.*\s)(?=.*[+*])[\S]{8,}$/;
