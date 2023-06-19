41bf8f0e8bc7b602708da598e21920207ac8922a
Gerardo Medrano <100704058+MedranoGerardo@users.noreply.github.com>
2023-06-18 18:06:28 -0600

    nuevos elementos

--- Js/_publicaciones.js
+++ Js/_publicaciones.js
@@ -1,7 +1,8 @@
 let cargar = JSON.parse(localStorage.getItem('post')) || [];
 
 export const cargarPost = () => {
-	const contenedor = document.querySelector('.publications');
+	const contenedor = document.querySelector('.publications--publicaciones');
+	const contenedor2 = document.querySelector('.publications--commet');
 	const mensaje = document.querySelector('.publications__content');
 
 	cargar.reverse();
@@ -16,46 +17,47 @@ export const cargarPost = () => {
                         <img src="${element.url}" alt="img">
                     </div>
 
-                    <h3 class="publications__title" title="Nombre de la pelicula">${element.titulo}</h3>
+                    <h3 class="publications__title">${element.titulo}</h3>
 
-                    <p class="publications__text" title="Descripcion de la pelicula">
+                    <p class="publications__text">
                         ${element.descripcion}
                     </p>
 
-                    <span class="publications__date" title="fecha de estreno">${element.fecha}</span>
+                    <span class="publications__date">${element.fecha}</span>
 
                     <hr class="publications__hr">
 
 					<div class="publications__creator">
 						<span class="publications__span">
-							<span class="publications__text" title="Publicacion creada por">${element.creador}</span>
+							<span class="publications__logo">logo</span>
+							<span class="publications__text">AlexPor</span>
 						</span>
-						<span class="publications__time" title="fecha de creacion de la publicacion">${element.fechaCreacion}</span>
+						<span class="publications__time">102728</span>
 					</div>
                 </div>
             `;
-			/********************************************************************************************** */
-			const CajaComentario = `
+			contenedor.innerHTML += plantilla;
+			
+		});
+		const CajaComentario = `
             <div class="publications__box">
+
+				<div class="comentarios__form">
+
+    			<textarea class="comentarios__text" placeholder="Escribe tu comentario"></textarea>
+    			
+				<button class="comentarios__submit">Enviar Comentario</button>
+    			<p class="comentarios__message"></p>
+  				</div>
+				
 				<h2>Comentarios</h2>
 				<div class="content">
 				<div class="comments-list-approved"></div>
+				
 				</div>
 
-				<div class="comentarios__form">
-    			<textarea class="comentarios__text" placeholder="Escribe tu comentario"></textarea>
-    			<button class="comentarios__submit">Enviar Comentario</button>
-    			<p class="comentarios-message"></p>
-  				</div>
 			</div>
             `;
-			/********************************************************************************************** */
-
-			contenedor.innerHTML += plantilla;
-
-			/********************************************************************************************** */
-			contenedor.innerHTML += CajaComentario;
-			/********************************************************************************************** */
-		});
+			contenedor2.innerHTML += CajaComentario;
 	}
 };
